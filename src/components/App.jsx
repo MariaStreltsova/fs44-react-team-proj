7813588ff9e7ece38b34ef76ce4dd91da07875c0
Vadim-kaR <raylyan.vadim@gmail.com>
2022-11-17 12:33:41 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101',
+        color: 'blue',
       }}
     >
       React team Project
