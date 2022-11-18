790cae608aa3844f55610adb6b2ea52ad5edecea
MariaStreltsova <Mariastreltova@gmail.com>
2022-11-18 10:31:47 +0200

    Revert "RRD"
    This reverts commit 66d02e558c3e88b2fc293f65e9d9e65d4a88c0d2.

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/fs44-react-team-proj/">
+        <BrowserRouter>
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
