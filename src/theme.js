0a403f01750605041db9d0318031303ce8ddcf53
sfirsovdev <s.firsov1985@gmail.com>
2022-11-18 22:06:37 +0300

    Добавил фонты+фавикон

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import '../src/stylesheet/fonts.css'
+
 const theme = {
   colors: {
     mainTxtColor: '#000',
@@ -12,7 +14,11 @@ const theme = {
     incomeColor: '#24CCA7',
     borderColor: 'rgba(224, 224, 224, 1)',
   },
-  fonts: {},
+    fontSizes: {
+    s: '12px',
+    m: '16px',
+    l: '18px',
+  },
   space: [0, 2, 4, 8, 16, 32, 64, 128, 256, 512],
   fontSizes: {
     xs: '12px',
