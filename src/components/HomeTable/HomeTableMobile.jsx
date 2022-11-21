95e3aafc7c2750bb910965deda07c98c29d810fd
Marya Samsonenko <samashushka7@gmail.com>
2022-11-21 18:47:06 +0200

    add HomeTable

--- src/components/HomeTable/HomeTableMobile.jsx
+++ src/components/HomeTable/HomeTableMobile.jsx
@@ -1,41 +1,51 @@
 import { transactions } from './mock';
+import {
+  TransactionsWrap,
+  TransactionCardList,
+  TransactionCardWrap,
+  TransactionCardItem,
+  ColoredSum,
+  CardsContainer,
+} from './HomeTable.styled';
 
 const HomeTableMobile = () => {
   return (
-    <ul>
-      {transactions.map(
-        ({ id, date, type, category, comments, sum, balanceAfter }) => (
-          <li key={id}>
-            <ul>
-              <li>
-                <span>Date</span>
-                <span>{date}</span>
-              </li>
-              <li>
-                <span>Type</span>
-                <span>{type}</span>
-              </li>
-              <li>
-                <span>Category</span>
-                <span>{category}</span>
-              </li>
-              <li>
-                <span>Comment</span>
-                <span>{comments}</span>
-              </li>
-              <li>
-                <span>Sum</span>
-                <span>{sum}</span>
-              </li>
-              <li>
-                <span>Balance</span>
-                <span>{balanceAfter}</span>
-              </li>
-            </ul>
-          </li>
-        )
-      )}
-    </ul>
+    <CardsContainer>
+      <TransactionsWrap>
+        {transactions.map(
+          ({ id, date, type, category, comments, amount, balanceAfter }) => (
+            <TransactionCardWrap key={id}>
+              <TransactionCardList type={type}>
+                <TransactionCardItem>
+                  <span>Date</span>
+                  <span>{date}</span>
+                </TransactionCardItem>
+                <TransactionCardItem>
+                  <span>Type</span>
+                  <span> {type === 'expense' ? '+' : '-'}</span>
+                </TransactionCardItem>
+                <TransactionCardItem>
+                  <span>Category</span>
+                  <span>{category}</span>
+                </TransactionCardItem>
+                <TransactionCardItem>
+                  <span>Comment</span>
+                  <span>{comments}</span>
+                </TransactionCardItem>
+                <TransactionCardItem>
+                  <span>Sum</span>
+                  <ColoredSum type={type}>{amount.toFixed(2)}</ColoredSum>
+                </TransactionCardItem>
+                <TransactionCardItem>
+                  <span>Balance</span>
+                  <span>{balanceAfter}</span>
+                </TransactionCardItem>
+              </TransactionCardList>
+            </TransactionCardWrap>
+          )
+        )}
+      </TransactionsWrap>
+    </CardsContainer>
   );
 };
 
