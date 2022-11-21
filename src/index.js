3225796d4fb694333616d23e47ae5b2aa3c240dc
Andrii Korchevnyi <91483336+andrey-korchevniy@users.noreply.github.com>
2022-11-21 10:11:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store, persistor } from './redux/store';
 import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+ <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter basename="/fs44-react-team-proj/">
@@ -19,5 +19,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </PersistGate>
   </Provider>
-  // {/* </React.StrictMode> */}
+ </React.StrictMode>
 );
