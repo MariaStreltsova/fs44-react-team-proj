cb4a896b4bfe610ae59b5bd781a4e7fc75ced809
sfirsovdev <s.firsov1985@gmail.com>
2022-11-24 20:01:48 +0300

    Модалка на закрытие (структура)

--- src/components/header/userMenu/UserMenu.jsx
+++ src/components/header/userMenu/UserMenu.jsx
@@ -2,9 +2,12 @@ import useAuth from "hooks/useAuht";
 import exit from "images/exit.svg";
 import { Box } from "Box";
 import { Text, Button, Icon } from "./UserMenu.styled";
+import { useState } from "react";
+import Modal from "../../Modal/Modal"
 
-const UserMenu = ({Modal}) => {
-    const {user}  = useAuth();
+const UserMenu = () => {
+    const { user } = useAuth();
+    const [modalActive, setModalActive] = useState(true)
     return (
         <Box display="flex" align-items="center">
             <Text>
@@ -12,14 +15,14 @@ const UserMenu = ({Modal}) => {
                 {user?.name}
             </Text>
             
-            <Button type='button' onClick={() => {
-            Modal()}}>
+        <Button type='button' onClick={() => setModalActive(true)}>
                 <Icon
                     src={exit}
                     alt="exit"
                 />
                 Exit
             </Button>
+            <Modal active={modalActive} setActive={setModalActive}/>
         </Box>
     )
 };
