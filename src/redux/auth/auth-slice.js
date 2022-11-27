e757a18376cc07295996d879ba7dc733125d8140
MariaStreltsova <Mariastreltova@gmail.com>
2022-11-27 14:10:07 +0200

    fixed Registration navigate to Login

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -15,7 +15,7 @@ const authSlice = createSlice({
     [authOperations.register.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
-      state.isLoggedIn = true;
+      // state.isLoggedIn = true;
     },
     [authOperations.logIn.fulfilled](state, action) {
       state.user = action.payload.user;
