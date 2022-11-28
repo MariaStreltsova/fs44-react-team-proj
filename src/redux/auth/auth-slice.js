1e10209b1cea2f0c9c1070e6119fd40ed535ea35
Vadim-kaR <raylyan.vadim@gmail.com>
2022-11-29 01:58:55 +0200

    comented reg slice

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -13,8 +13,8 @@ const authSlice = createSlice({
   initialState,
   extraReducers: {
     [authOperations.register.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
+      // state.user = action.payload.user;
+      // state.token = action.payload.token;
       // state.isLoggedIn = true;
     },
     [authOperations.logIn.fulfilled](state, action) {
